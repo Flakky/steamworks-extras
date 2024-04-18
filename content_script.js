d9503e6603992c030521f00d00dffec0442f2492
AlexBlintsov <al.flakky@gmail.com>
2024-04-18 22:12:52 +0400

    logs cleanup + es6

--- content_script.js
+++ content_script.js
@@ -1,27 +1,60 @@
-var numberWithCommas = (x) => {
+const numberWithCommas = (x) => {
     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","); // https://stackoverflow.com/questions/2901102/how-to-format-a-number-with-commas-as-thousands-separators
 }
 
-var addSummaryNetRow = () => {
-    var table = getSummaryTable();
-    if (!table) return; // If no table is found, exit the function
+const getSummaryTable = () => {
+    return document.querySelector('.lifetimeSummaryCtn table');
+}
 
-    console.log(table);
+const getSalesTable = () => {
+    var parentElement = document.getElementById('gameDataLeft');
 
-    var rows = table.rows;
-    var netRevenueCell = rows[1].cells[1];
-    var netRevenue = netRevenueCell.textContent;
+    var childElements = parentElement.children;
+    var divs = [];
+
+    // Filter out only those children that are divs
+    for (var i = 0; i < childElements.length; i++) {
+        if (childElements[i].tagName === 'DIV') {
+            divs.push(childElements[i]);
+        }
+    }
+
+    const salesDiv = divs[3];
+
+    return salesDiv.getElementsByTagName('table')[0];
+}
+
+const getPackageId = () => {
+    const salesTable = getSalesTable();
+
+    const rows = salesTable.rows;
+
+    const packageRow = rows[2];
+
+    const packageLink = packageRow.getElementsByTagName('a')[0];
+
+    const id = packageLink.href.match(/\/package\/details\/(\d+)/)[1];
+
+    return id;
+}
+
+const addSummaryNetRow = () => {
+    const table = getSummaryTable();
+    if (!table) return;
+
+    const rows = table.rows;
+    const netRevenueCell = rows[1].cells[1];
+
+    let netRevenue = netRevenueCell.textContent;
     netRevenue = netRevenue.replace('$', '');
     netRevenue = netRevenue.replace(',', '');
-    console.log(Number(netRevenue))
-    var devRevenue = Number(netRevenue * 0.7);
-    var devRevenueString = numberWithCommas(Math.floor(devRevenue));
+    const devRevenueString = numberWithCommas(Math.floor(netRevenue * 0.7));
 
-    var newRow = table.insertRow(2); // Insert after net
+    const newRow = table.insertRow(2); // Insert after net
 
-    var nameElem = document.createElement('td');
-    var sumElem = document.createElement('td');
-    var descElem = document.createElement('td');
+    const nameElem = document.createElement('td');
+    const sumElem = document.createElement('td');
+    const descElem = document.createElement('td');
     descElem.textContent = '(Net revenue - Steam revenue share)'
     nameElem.textContent = 'Lifetime developer revenue share';
     sumElem.textContent = `$${devRevenueString}`
@@ -30,24 +63,19 @@ var addSummaryNetRow = () => {
     newRow.appendChild(sumElem);
     newRow.appendChild(descElem);
 
-    console.log("Steamworks Dev Net: Added summary net");
+    console.log("Steamworks extras: Added summary net");
 }
 
-var addSalesNetRow = () => {
-    var salesTable = getSalesTable();
+const addSalesNetRow = () => {
+    const salesTable = getSalesTable();
 
-    var rows = salesTable.rows;
-
-    console.log(rows);
+    const rows = salesTable.rows;
 
     let revenueRow = undefined;
     let revenueRowIndex = -1;
     
     for(const row of rows) {
         revenueRowIndex++;
-        const rowName = row.getElementsByTagName('td')[0];
-
-        console.log(rowName.textContent);
 
         const bElemWithText = row.getElementsByTagName('b')[0];
         if(!bElemWithText) continue;
@@ -63,19 +91,17 @@ var addSalesNetRow = () => {
         return;
     }
 
-    var revenueCell = revenueRow.cells[2];
-    var revenue = revenueCell.textContent;
+    const revenueCell = revenueRow.cells[2];
+    let revenue = revenueCell.textContent;
     revenue = revenue.replace('$', '');
     revenue = revenue.replace(',', '');
-    console.log(Number(revenue))
-    var devRevenue = Number(revenue * 0.53);
-    var devRevenueString = numberWithCommas(Math.floor(devRevenue));
+    const devRevenueString = numberWithCommas(Math.floor(revenue * 0.53));
 
-    var newRow = salesTable.insertRow(revenueRowIndex+1); // Insert after total revenue
+    const newRow = salesTable.insertRow(revenueRowIndex+1); // Insert after total revenue
 
-    var nameElem = document.createElement('td');
-    var sumElem = document.createElement('td');
-    var spacerElem = document.createElement('td');
+    const nameElem = document.createElement('td');
+    const sumElem = document.createElement('td');
+    const spacerElem = document.createElement('td');
     nameElem.innerHTML = '<b>Developer revenue share</b>';
     sumElem.innerHTML = `<b>~$${devRevenueString}</b>`;
     sumElem.setAttribute('align', 'right');
@@ -85,67 +111,28 @@ var addSalesNetRow = () => {
     newRow.appendChild(sumElem);
 
     for(let i = 3; i<revenueRow.cells.length;i++){
-        var cell = document.createElement('td');
+        const cell = document.createElement('td');
         newRow.appendChild(cell);
     }
 
-    console.log("Steamworks Dev Net: Added sales for date range net");
+    console.log("Steamworks extras: Added sales for date range net");
 }
 
-const getSummaryTable = () => {
-    return document.querySelector('.lifetimeSummaryCtn table');
-}
-
-var getSalesTable = () => {
-    var parentElement = document.getElementById('gameDataLeft');
-
-    var childElements = parentElement.children;
-    var divs = [];
-
-    // Filter out only those children that are divs
-    for (var i = 0; i < childElements.length; i++) {
-        if (childElements[i].tagName === 'DIV') {
-            divs.push(childElements[i]);
-        }
-    }
-
-    var salesDiv = divs[3];
-
-    console.log(salesDiv);
-
-    return salesDiv.getElementsByTagName('table')[0];
-}
-
-var getPackageId = () => {
-    const salesTable = getSalesTable();
-
-    const rows = salesTable.rows;
-
-    const packageRow = rows[2];
-
-    const packageLink = packageRow.getElementsByTagName('a')[0];
-
-    const id = packageLink.href.match(/\/package\/details\/(\d+)/)[1];
-
-    return id;
-}
-
-var addRefundDataLink = () => {
+const addRefundDataLink = () => {
     const summaryTable = getSummaryTable();
-    if (!summaryTable) return; // If no table is found, exit the function
+    if (!summaryTable) return;
 
-    console.log(summaryTable);
-
-    var rows = summaryTable.rows;
-    var refundCell = rows[7].cells[2];
+    const rows = summaryTable.rows;
+    const refundCell = rows[7].cells[2];
 
     const packageId = getPackageId();
-    console.log(`Package id: ${packageId}`);
 
     refundCell.innerHTML += ` (<a href="https://partner.steampowered.com/package/refunds/${packageId}/">Refund data</a>)`;
+
+    console.log("Steamworks extras: Added refund data link");
 }
 
-console.log("Steamworks Dev Net: Begin");
+console.log("Steamworks extras: Begin");
 
 addSummaryNetRow();
 addSalesNetRow();
