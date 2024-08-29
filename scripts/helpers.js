895d57fa6009a70915c109c3acbe0f96f3044640
AlexBlintsov <al.flakky@gmail.com>
2024-08-30 01:44:37 +0400

    Helpers documentation comments

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,13 +1,46 @@
 let helpers = {}
 
+/**
+ * Returns number splitted with commas as thousands separators
+ *
+ * @param {number} x - Number to format
+ * @returns {string} - formatted string of given number.
+ *
+ * @example
+ * // returns 123,456,789
+ * numberWithCommas(123456789);
+ */
 helpers.numberWithCommas = (x) => {
-  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","); // https://stackoverflow.com/questions/2901102/how-to-format-a-number-with-commas-as-thousands-separators
+  // https://stackoverflow.com/questions/2901102/how-to-format-a-number-with-commas-as-thousands-separators
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
 
+/**
+ * Checks if given string is empty
+ *
+ * @param {string} x - String to check
+ * @returns {boolean} - True if string is empty, undefined or null
+ *
+ * @example
+ * // returns true
+ * isStringEmpty(' ');
+ */
 helpers.isStringEmpty = (str) => {
   return str === null || str === undefined || str.trim() === '';
 }
 
+/**
+ * Find an element which contains certain text on a page (DOM)
+ *
+ * @param {string} tag - Tag which should contain the text
+ * @param {string} text - Text to look for
+ * @param {string} doc - [Optional] DOM object to look inside. If undefined, then will use current page document
+ * @returns {object} - Returns DOM Element with text.
+ *
+ * @example
+ * // returns {...}
+ * findElementByText('td', 'Hello World', document);
+ */
 helpers.findElementByText = (tag, text, doc = undefined) => {
   const elements = doc ? doc.getElementsByTagName(tag) : document.getElementsByTagName(tag);
 
@@ -20,6 +53,17 @@ helpers.findElementByText = (tag, text, doc = undefined) => {
   return undefined;
 }
 
+/**
+ * Find the first element of the given tag in the parent chain, starting from a given element.
+ *
+ * @param {object} element - DOM element to start searching from
+ * @param {string} tagName - Tag of the element to look for
+ * @returns {object} - Returns DOM Element
+ *
+ * @example
+ * // returns table element which contains td cell
+ * findParentByTag(myTableTDCellElem, 'table');
+ */
 helpers.findParentByTag = (element, tagName) => {
 
   tagName = tagName.toUpperCase();
@@ -34,6 +78,19 @@ helpers.findParentByTag = (element, tagName) => {
   return undefined;
 }
 
+/**
+ * Asyncroniosly request specific country revenue of given date rage.
+ *
+ * @param {string} appID - AppID of the game
+ * @param {string} country - Country which revenue to request of
+ * @param {string} dateStart - [Optional] Start date. 2010-01-01 if not provided
+ * @param {string} dateEnd - [Optional] End date. Today if not provided
+ * @returns {Promise<number>} - Promise with revenue (number)
+ *
+ * @example
+ * // returns 123456
+ * await getCountryRevenue(000000, 'United States', Date('2020-01-20'), Date('2021-05-20')));
+ */
 helpers.getCountryRevenue = async (appID, country, dateStart, dateEnd) => {
 
   const startDate = dateStart || new Date(2010, 0, 1);
