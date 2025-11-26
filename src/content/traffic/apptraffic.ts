4ae8d16b100e1b076eb356724efddce96bbefa61
a.blintsov <al.flakky@gmail.com>
2025-11-26 15:30:54 +0800

    apptraffic page to typed traffic data

--- src/content/traffic/apptraffic.ts
+++ src/content/traffic/apptraffic.ts
@@ -1,113 +1,114 @@
 import { getDefaultSettings, readChartColors } from "../site";
 import { addStatusBlockToPage } from "../../shared/statusblock";
-import { DateRange, getDateRangeArray } from "../../shared/types/daterange";
+import { DateRange } from "../../shared/types/daterange";
 import { hideOldElements } from "./layout";
-import { getDataFromStorage } from "../../scripts/helpers";
+import { dateToString, getDataFromStorage } from "../../scripts/helpers";
 import { TrafficCategorySelection, TrafficChartDataType, TrafficPresetType } from "./types";
 import { createChart, updateTrafficChart } from "./chart";
 import { addTableCheckboxes, getExternalWebsiteSubcategories, getTopCategories, updateSelectedChartCategories } from "./table";
 import { createCheckPresets } from "./presets";
+import { GameTraffic } from "../../shared/types/traffic";
 
 const init = async () => {
-  console.log("Init");
+    console.log("Init");
 
-  const settings = await getDefaultSettings();
-  if (!settings) {
-    throw new Error('Settings not found');
-  }
-
-  const chartColors = await readChartColors();
-  if (!chartColors) {
-    throw new Error('Chart colors not found');
-  }
-
-  const appID = getAppID();
-  if (!appID) {
-    throw new Error('App ID not found');
-  }
-
-  const doc = document;
-
-  const trafficData = await getTrafficData(doc, appID);
-  if (!trafficData) {
-    throw new Error('Traffic data not found');
-  }
-
-  hideOldElements(doc);
-
-  const categorySelection = new TrafficCategorySelection();
-
-  const trafficChart = createChart(doc, trafficData, TrafficChartDataType.Impressions, categorySelection, chartColors);
-
-  addTableCheckboxes(doc, categorySelection, (catSelection: TrafficCategorySelection) => {
-    updateTrafficChart(doc, trafficData, trafficChart, TrafficChartDataType.Impressions, catSelection, chartColors);
-  });
-
-
-  addStatusBlockToPage();
-
-  createCheckPresets(doc, (preset: TrafficPresetType) => {
-    switch (preset) {
-      case TrafficPresetType.Clear:
-        categorySelection.categories = [];
-        categorySelection.subcategories = [];
-        break;
-      case TrafficPresetType.Top5:
-        categorySelection.categories = getTopCategories(doc, 5);
-        break;
-      case TrafficPresetType.Top10:
-        categorySelection.categories = getTopCategories(doc, 10);
-        break;
-      case TrafficPresetType.External:
-        categorySelection.subcategories = getExternalWebsiteSubcategories(doc).map(subcategory => ({ category: 'External', subCategory: subcategory }));
-        break;
+    const settings = await getDefaultSettings();
+    if (!settings) {
+        throw new Error('Settings not found');
     }
 
-    updateSelectedChartCategories(doc, categorySelection);
-    updateTrafficChart(doc, trafficData, trafficChart, TrafficChartDataType.Impressions, categorySelection, chartColors);
-  });
+    const chartColors = await readChartColors();
+    if (!chartColors) {
+        throw new Error('Chart colors not found');
+    }
+
+    const appID = getAppID();
+    if (!appID) {
+        throw new Error('App ID not found');
+    }
+
+    const doc = document;
+
+    const trafficData = await getTrafficData(doc, appID);
+    if (!trafficData) {
+        throw new Error('Traffic data not found');
+    }
+
+    hideOldElements(doc);
+
+    const categorySelection = new TrafficCategorySelection();
+
+    const trafficChart = createChart(doc, trafficData, TrafficChartDataType.Impressions, categorySelection, chartColors);
+
+    addTableCheckboxes(doc, categorySelection, (catSelection: TrafficCategorySelection) => {
+        updateTrafficChart(doc, trafficData, trafficChart, TrafficChartDataType.Impressions, catSelection, chartColors);
+    });
+
+
+    addStatusBlockToPage();
+
+    createCheckPresets(doc, (preset: TrafficPresetType) => {
+        switch (preset) {
+            case TrafficPresetType.Clear:
+                categorySelection.categories = [];
+                categorySelection.subcategories = [];
+                break;
+            case TrafficPresetType.Top5:
+                categorySelection.categories = getTopCategories(doc, 5);
+                break;
+            case TrafficPresetType.Top10:
+                categorySelection.categories = getTopCategories(doc, 10);
+                break;
+            case TrafficPresetType.External:
+                categorySelection.subcategories = getExternalWebsiteSubcategories(doc).map(subcategory => ({ category: 'External', subCategory: subcategory }));
+                break;
+        }
+
+        updateSelectedChartCategories(doc, categorySelection);
+        updateTrafficChart(doc, trafficData, trafficChart, TrafficChartDataType.Impressions, categorySelection, chartColors);
+    });
 }
 
 const getAppID = () => {
-  const url = window.location.href;
+    const url = window.location.href;
 
-  // Regular expression to extract the appID
-  const regex = /navtrafficstats\/(\d+)/;
-  const match = url.match(regex);
+    // Regular expression to extract the appID
+    const regex = /navtrafficstats\/(\d+)/;
+    const match = url.match(regex);
 
-  if (!match) return;
+    if (!match) return;
 
-  return match[1];
+    return match[1];
 }
 
 export const getDateRangeOfCurrentPage = (doc: Document): DateRange => {
-  const startDateElem = doc.getElementById('start_date') as HTMLInputElement;
-  const endDateElem = doc.getElementById('end_date') as HTMLInputElement;
+    const startDateElem = doc.getElementById('start_date') as HTMLInputElement;
+    const endDateElem = doc.getElementById('end_date') as HTMLInputElement;
 
-  if (!startDateElem || !endDateElem) {
-    throw new Error('Start or end date element not found');
-  }
+    if (!startDateElem || !endDateElem) {
+        throw new Error('Start or end date element not found');
+    }
 
-  const dateStart = new Date(startDateElem.value);
-  const dateEnd = new Date(endDateElem.value);
+    const dateStart = new Date(startDateElem.value);
+    const dateEnd = new Date(endDateElem.value);
 
-  return { dateStart, dateEnd };
+    return { dateStart, dateEnd };
 }
 
-const getTrafficData = async (doc: Document, appID: string): Promise<any[]> => {
-  console.log("Requesting traffic data");
+const getTrafficData = async (doc: Document, appID: string): Promise<GameTraffic[]> => {
+    console.log("Requesting traffic data");
 
-  const { dateStart, dateEnd } = getDateRangeOfCurrentPage(doc);
+    const { dateStart, dateEnd } = getDateRangeOfCurrentPage(doc);
 
-  const trafficData = await getDataFromStorage(
-    'Traffic',
-    appID,
-    dateStart,
-    dateEnd,
-    true
-  );
+    const trafficData = await getDataFromStorage(
+        'Traffic',
+        appID,
+        dateToString(dateStart),
+        dateToString(dateEnd),
+        true
+    );
 
-  return trafficData;
+    return trafficData as GameTraffic[];
 }
 
 init();
--- src/content/traffic/chart.ts
+++ src/content/traffic/chart.ts
@@ -4,148 +4,163 @@ import { getPageContentElem } from "./layout";
 import { Chart, ChartConfiguration, ChartDataset } from "chart.js";
 import { selectChartColor } from "../../scripts/helpers";
 import { getDateRangeOfCurrentPage } from "./apptraffic"
+import { GameTraffic } from "../../shared/types/traffic";
 
-export const createChart = (doc: Document, trafficData: any[], dataType: TrafficChartDataType, categorySelection: TrafficCategorySelection, chartColors: Record<string, string>): Chart => {
-  const chartBlockElem = doc.createElement('div');
-  chartBlockElem.id = 'extras_chart';
+export const createChart = (doc: Document, trafficData: GameTraffic[], dataType: TrafficChartDataType, categorySelection: TrafficCategorySelection, chartColors: Record<string, string>): Chart => {
+    const chartBlockElem = doc.createElement('div');
+    chartBlockElem.id = 'extras_chart';
 
-  const pageContentElem = getPageContentElem(doc);
-  pageContentElem.insertBefore(chartBlockElem, pageContentElem.children[18]);
+    const pageContentElem = getPageContentElem(doc);
+    pageContentElem.insertBefore(chartBlockElem, pageContentElem.children[18]);
 
-  const createChartSelect = (options: string[], name: string, type: TrafficChartDataType, onSelect: (select: HTMLSelectElement) => void) => {
-    const nameElem = document.createElement("b");
-    nameElem.textContent = `${name}: `;
-    nameElem.classList.add('extra_chart_select_name');
+    const createChartSelect = (options: string[], name: string, type: TrafficChartDataType, onSelect: (select: HTMLSelectElement) => void) => {
+        const nameElem = document.createElement("b");
+        nameElem.textContent = `${name}: `;
+        nameElem.classList.add('extra_chart_select_name');
 
-    const selectElem = document.createElement("select");
+        const selectElem = document.createElement("select");
 
-    options.forEach(option => {
-      const optionElement = document.createElement("option");
-      optionElement.value = option;
-      optionElement.textContent = option;
-      selectElem.appendChild(optionElement);
-    });
+        options.forEach(option => {
+            const optionElement = document.createElement("option");
+            optionElement.value = option;
+            optionElement.textContent = option;
+            selectElem.appendChild(optionElement);
+        });
 
-    selectElem.value = type;
+        selectElem.value = type;
 
-    selectElem.addEventListener("change", () => { onSelect(selectElem); });
+        selectElem.addEventListener("change", () => { onSelect(selectElem); });
 
-    chartBlockElem.appendChild(nameElem);
-    chartBlockElem.appendChild(selectElem);
+        chartBlockElem.appendChild(nameElem);
+        chartBlockElem.appendChild(selectElem);
 
-    return selectElem;
-  }
+        return selectElem;
+    }
 
-  const canvas = document.createElement('canvas');
-  canvas.id = 'extra_trafficChart';
-  canvas.width = 800;
-  canvas.height = 400;
+    const canvas = document.createElement('canvas');
+    canvas.id = 'extra_trafficChart';
+    canvas.width = 800;
+    canvas.height = 400;
 
-  chartBlockElem.appendChild(canvas);
+    chartBlockElem.appendChild(canvas);
 
-  const config: ChartConfiguration = {
-    type: 'line',
-    data: { datasets: [], labels: [] },
-    options: {
-      plugins: {
-        legend: {
-          position: 'top'
+    const config: ChartConfiguration = {
+        type: 'line',
+        data: { datasets: [], labels: [] },
+        options: {
+            plugins: {
+                legend: {
+                    position: 'top'
+                }
+            }
         }
-      }
-    }
-  };
-
-  const trafficChart = new Chart(canvas, config);
-
-  createChartSelect(
-    Object.values(TrafficChartDataType).map(type => type),
-    'View by',
-    dataType,
-    (select) => {
-      console.log(select.value);
-      const selectedDataType = select.value as TrafficChartDataType;
-      updateTrafficChart(doc, trafficData, trafficChart, selectedDataType, categorySelection, chartColors);
-    }
-  );
-
-  return trafficChart;
-}
-
-export const updateTrafficChart = (doc: Document, trafficData: any[], chart: Chart, dataType: TrafficChartDataType, categorySelection: TrafficCategorySelection, chartColors: Record<string, string>) => {
-  const dateRange = getDateRangeOfCurrentPage(doc);
-
-  const days = getDateRangeArray(dateRange, false, true);
-  console.log(days);
-
-  const chartData: Record<string, { impressions: number, visits: number }[]> = {};
-
-  const addDataToMap = (tag: string, date: string, value: { impressions: number, visits: number }) => {
-    if (chartData[tag] === undefined) chartData[tag] = Array.from(
-      { length: days.length },
-      () => ({ impressions: 0, visits: 0 }));
-
-    const dateIndex = days.indexOf(date);
-    if (dateIndex < 0) return;
-
-    chartData[tag][dateIndex].impressions += value.impressions;
-    chartData[tag][dateIndex].visits += value.visits;
-  }
-
-  for (const data of trafficData) {
-    const value = {
-      impressions: data['Impressions'],
-      visits: data['Visits']
     };
 
-    const date = data['Date'];
+    const trafficChart = new Chart(canvas, config);
 
-    if (categorySelection.categories.length === 0 && categorySelection.subcategories.length === 0) {
-      addDataToMap('Total', date, value);
+    createChartSelect(
+        Object.values(TrafficChartDataType).map(type => type),
+        'View by',
+        dataType,
+        (select) => {
+            console.log(select.value);
+            const selectedDataType = select.value as TrafficChartDataType;
+            updateTrafficChart(doc, trafficData, trafficChart, selectedDataType, categorySelection, chartColors);
+        }
+    );
+
+    return trafficChart;
+}
+
+export const updateTrafficChart = (doc: Document, trafficData: GameTraffic[], chart: Chart, dataType: TrafficChartDataType, categorySelection: TrafficCategorySelection, chartColors: Record<string, string>) => {
+    const dateRange = getDateRangeOfCurrentPage(doc);
+
+    const days = getDateRangeArray(dateRange, false, true) as string[];
+
+    const chartData = createChartData(trafficData, categorySelection, days);
+
+    const datasets: ChartDataset[] = [];
+
+    for (const [key, value] of Object.entries(chartData)) {
+
+        const values: number[] = value.map(val => {
+            switch (dataType) {
+                case TrafficChartDataType.Impressions: return val.impressions;
+                case TrafficChartDataType.Visits: return val.visits;
+                case TrafficChartDataType.ClickThroughRate:
+                    const dataValue = Number((val.visits / val.impressions * 100.0).toFixed(2));
+                    return isNaN(dataValue) ? 0.0 : dataValue;
+            }
+        });
+
+        console.log(values);
+
+        const color = selectChartColor(chartColors, key);
+
+        datasets.push({
+            label: key,
+            data: values,
+            fill: false,
+            backgroundColor: color,
+            borderColor: color,
+            tension: 0
+        });
     }
 
-    for (const category of categorySelection.categories) {
-      if (data['PageCategory'] !== category) continue;
+    chart.data.labels = days;
+    chart.data.datasets = datasets;
 
-      addDataToMap(category, date, value);
+    chart.update();
+}
+
+const createChartData = (trafficData: GameTraffic[], categorySelection: TrafficCategorySelection, days: string[]): Record<string, { impressions: number, visits: number }[]> => {
+    const chartData: Record<string, { impressions: number, visits: number }[]> = {};
+
+    const addDataToMap = (tag: string, date: string, value: { impressions: number, visits: number }) => {
+        if (chartData[tag] === undefined) chartData[tag] = Array.from(
+            { length: days.length },
+            () => ({ impressions: 0, visits: 0 }));
+
+        const dateIndex = days.indexOf(date);
+        if (dateIndex < 0) return;
+
+        chartData[tag][dateIndex].impressions += value.impressions;
+        chartData[tag][dateIndex].visits += value.visits;
     }
 
-    for (const category of categorySelection.subcategories) {
-      if (data['PageCategory'] !== category.category || data['PageFeature'] !== category.subCategory) continue;
+    const totalSelection = categorySelection.categories.length === 0 && categorySelection.subcategories.length === 0;
 
-      addDataToMap(`${category.category} | ${category.subCategory}`, date, value);
+    for (const data of trafficData) {
+        if (totalSelection) {
+            const totalValue = {
+                impressions: 0,
+                visits: 0
+            };
+            for (const category of Object.keys(data.categories)) {
+                totalValue.impressions += data.categories[category].impressions;
+                totalValue.visits += data.categories[category].visits;
+            }
+            addDataToMap('Total', data.date, totalValue);
+        }
+        for (const category of Object.keys(data.categories)) {
+            for (const feature of Object.keys(data.categories[category].featureTraffic)) {
+                if (categorySelection.categories.includes(category)) {
+                    const value = {
+                        impressions: data.categories[category].impressions,
+                        visits: data.categories[category].visits
+                    };
+                    addDataToMap(category, data.date, value);
+                }
+                if (categorySelection.subcategories.some(subcategory => subcategory.category === category && subcategory.subCategory === feature)) {
+                    const value = {
+                        impressions: data.categories[category].featureTraffic[feature].impressions,
+                        visits: data.categories[category].featureTraffic[feature].visits
+                    };
+                    addDataToMap(`${category} | ${feature}`, data.date, value);
+                }
+            }
+        }
     }
-  }
 
-  const datasets: ChartDataset[] = [];
-
-  for (const [key, value] of Object.entries(chartData)) {
-
-    const values: number[] = value.map(val => {
-      switch (dataType) {
-        case TrafficChartDataType.Impressions: return val.impressions;
-        case TrafficChartDataType.Visits: return val.visits;
-        case TrafficChartDataType.ClickThroughRate:
-          const dataValue = Number((val.visits / val.impressions * 100.0).toFixed(2));
-          return isNaN(dataValue) ? 0.0 : dataValue;
-      }
-    });
-
-    console.log(values);
-
-    const color = selectChartColor(chartColors, key);
-
-    datasets.push({
-      label: key,
-      data: values,
-      fill: false,
-      backgroundColor: color,
-      borderColor: color,
-      tension: 0
-    });
-  }
-
-  chart.data.labels = days;
-  chart.data.datasets = datasets;
-
-  chart.update();
+    return chartData;
 }
